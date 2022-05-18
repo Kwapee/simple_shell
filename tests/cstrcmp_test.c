1bba703e99d5dc22c4f53a1e82c8691c174980e9
root <p.amoako2002@gmail.com>
2022-05-18 07:04:28 -0700

    cool

---
+++ tests/cstrcmp_test.c
@@ -0,0 +1,29 @@
+#include "../header.h"
+
+/*
+* gcc -Wall -Werror -Wextra -pedantic
+* ../string.c ../string2.c cstrcmp_test.c && ./a.out
+*/
+
+int main(void)
+{
+	char *s1, *s2;
+
+	printf("----------\n");
+
+	s1 = strdup("abc");
+	s2 = strdup("abcde");
+	printf("comparing: \"%s\" and \"%s\"\n", s1, s2);
+	printf("result: %d\n", _cstrcmp(s1, s2));
+	printf("expected: 0\n");
+
+	printf("----------\n");
+
+	s1 = strdup("aa");
+	s2 = strdup("ab");
+	printf("comparing: \"%s\" and \"%s\"\n", s1, s2);
+	printf("result: %d\n", _cstrcmp(s1, s2));
+	printf("expected: not 0\n");
+
+	return (0);
+}
